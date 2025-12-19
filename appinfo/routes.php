2fd7e159ce780c3cda25ee3d0f25e365a89e5915
Tom Wezepoel <tom.wezepoel@surf.nl>
2025-12-19 22:54:49 +0100

    Fix Admin interface and add OCS api

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -8,5 +8,14 @@ return [
         ['name' => 'section#destroy', 'url' => '/api/sections/{id}', 'verb' => 'DELETE'],
         ['name' => 'section#reorder', 'url' => '/api/sections/reorder', 'verb' => 'POST'],
         ['name' => 'section#saveSettings', 'url' => '/api/settings', 'verb' => 'POST'],
+    ],
+    'ocs' => [
+        ['name' => 'api#getSections', 'url' => '/api/v1/sections', 'verb' => 'GET'],
+        ['name' => 'api#getSection', 'url' => '/api/v1/sections/{id}', 'verb' => 'GET'],
+        ['name' => 'api#createSection', 'url' => '/api/v1/sections', 'verb' => 'POST'],
+        ['name' => 'api#updateSection', 'url' => '/api/v1/sections/{id}', 'verb' => 'PUT'],
+        ['name' => 'api#deleteSection', 'url' => '/api/v1/sections/{id}', 'verb' => 'DELETE'],
+        ['name' => 'api#getSettings', 'url' => '/api/v1/settings', 'verb' => 'GET'],
+        ['name' => 'api#updateSettings', 'url' => '/api/v1/settings', 'verb' => 'PUT'],
     ]
 ];
